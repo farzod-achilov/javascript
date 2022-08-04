ae515d0467370695ea25ed6aaf054856063ad5f5
farzod.achilov <farzodachililov27@gmail.com>
2022-08-04 11:06:07 +0500

    javascript

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var firstName = prompt("Ismingiz");
+console.log(firstName);
+
+var lastName = prompt("Familangiz");
+console.log(lastName);
+
+var age = prompt("Yoshingiz");
+console.log(age);
+
+var yearOfBirth = prompt("Tugilgan yilingiz");
+console.log(yearOfBirth);
+
+var birthDay = prompt("Tugilgan kuningiz");
+console.log(birthDay);
+
+var whatDoYoKnow = prompt("Qaysi tillarni bilasiz");
+console.log(whatDoYoKnow);
+
+var yourPhone = prompt("Telefon raqamingiz");
+console.log(yourPhone);
+
+var yourCountry = prompt("Qayshi davlatdansiz");
+console.log(yourCountry);
+
+var yourCity = prompt("Qaysi Shahardansiz");
+console.log(yourCity);
+
+var yourSchool = prompt("Qayerda o`qigansiz");
+console.log(yourSchool);
